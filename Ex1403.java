69c16b131ef706f2c6d135bc2cf8949ba614d12a
hvxxn <hoopa72056@icloud.com>
2024-06-08 02:51:09 +0900

    1

--- Ex1403.java
+++ Ex1403.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Ex1404 extends JFrame {
+public class Ex1403 extends JFrame {
 
-public Ex1404() {
+public Ex1403() {
 setTitle("계산기");
 setSize(300, 250);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 JPanel titlePanel = new JPanel() ;
 titlePanel.setBounds(0, 0, 300, 40);
@@ -16,9 +17,6 @@ titlePanel.add(title);
 
 JPanel numPanel = new JPnel();
 numPanel.setBounds(0, 40, 300, 40);
-
-numPanel.setLayout(new FlowLayout(flowLayout.CENTER));
-
 add(numPanel);
 JTextField num1 = new JTextField(5);
 numPanel.add(num1);
@@ -26,16 +24,13 @@ JTextField num2 = new JTextField(5);
 numPanel.add(num2);
 }
 
-JPanel btnPanel = new JPnel();
-btnPanel.setBounds(0, 80, 300, 40);
-
-btnPaner.setLayout(new GrindLayout(2, 2, 10, 5));
-
-add(btnPanel);
+JPanel btnPanel1 = new JPnel();
+btnPanel1.setBounds(0, 80, 300, 40);
+add(btnPanel1);
 JButton plus = new JButton("+");
-btnPanel.add(plus);
+btnPanel1.add(plus);
 JButton minus = new JButton("-");
-btnPanel.add(minus);
+btnPanel1.add(minus);
 
 JPanel btnPanel2 = new JPnel();
 btnPanel2.setBounds(0, 80, 300, 40);
@@ -58,5 +53,5 @@ setLayout(null);
 setVisible(true);
 }
 public static void main(String[] args){
-new Ex1404();
+new Ex1403();
 }
